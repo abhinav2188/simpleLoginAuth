8573c4494e9de897620294102572740a63593bf9
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-02-22 02:54:37 +0530

    login using google

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose');
 const session = require("express-session");
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 app.use(session({
   secret: 'keyboard cat',
@@ -25,15 +27,39 @@ app.set('view engine', 'ejs');
 app.use(passport.initialize());
 app.use(passport.session());
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String,
+  googleId:String
+});
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model('user',userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/success"
+},
+function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}
+));
 
 // default route - get
 app.get('/',function(req,res){
@@ -53,6 +79,16 @@ app.get('/success',function(req,res){
     res.redirect('/login');
   }
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/success',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
 
 app.post('/register',function(req,res){
   User.register({username:req.body.email}, req.body.password, function(err, user) {
