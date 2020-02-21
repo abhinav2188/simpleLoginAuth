d6e0ae7ddc276457395fceacba33e3e872a98ebb
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-02-21 18:35:01 +0530

    login using passport

--- app.js
+++ app.js
@@ -3,34 +3,59 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10);
+
+const passport = require('passport')
+// const LocalStrategy = require('passport-local').Strategy;
+const passportLocalMongoose = require('passport-local-mongoose');
+const session = require("express-session");
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 mongoose.connect('mongodb://localhost/usersDB',{ useUnifiedTopology: true , useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(express.static('public/'));
-
 app.set('view engine', 'ejs');
 
-const userSchema = new mongoose.Schema({
-    email:{
-        required:true,
-        type:String
-    },
-    password:{
-        required:true,
-        type:String
-    }
-});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport.use(new LocalStrategy({username:'email'},
+//   function(username, password, done) {
+//     console.log("in loacl strategy");
+//     User.findOne({ email: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+const userSchema = new mongoose.Schema({});
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('user',userSchema);
 
-let defaultUser = new User({
-    email:"abhinav.26aug@gmail.com",
-    password:"gmail@com"
-});
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// let defaultUser = new User({
+//     email:"abhinav.26aug@gmail.com",
+//     password:"gmail@com"
+// });
 
 // defaultUser.save();
 
@@ -38,48 +63,59 @@ let defaultUser = new User({
 app.get('/',function(req,res){
     res.render('home');
 });
-
 app.get('/register',function(req,res){
     res.render('register');
 });
 app.get('/login',function(req,res){
     res.render('login');
 });
-app.post('/register',function(req,res){
-   let newUser = new User({
-       email: req.body.email,
-       password : bcrypt.hashSync(req.body.password, salt)
-   }) ;
-   newUser.save(function(err){
-       if(!err){
-           console.log('new user saved');
-           res.redirect('/');
-       }else{
-           res.send(err);
-       }
-   })
+app.get('/secrets',function(req,res){
+  console.log(req);
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }
+  else{
+    res.render('failure',{msg:'Error in redirecting secrets / authentication'})
+  }
 });
+
+app.post('/register',function(req,res){
+  User.register({username:req.body.email}, req.body.password, function(err, user) {
+    if (err) {
+      res.render('failure',{msg:err});
+    }
+    res.redirect('/login');
+    // var authenticate = passport.authenticate('local');
+    // authenticate(req,res,function(){
+    //   res.redirect('/secrets');
+    // });
+    // var authenticate = User.authenticate();
+    // authenticate(req.body.email, req.body.password, function(err, result) {
+    //   if (err) {
+    //   res.render('failure',{err:'error in authentication'}) }
+    //   else{
+    //     res.redirect('/secrets');
+    //   }
+    // });
+  });
+});
+
 app.post('/login',function(req,res){
-    User.findOne({email:req.body.email} , function(err,foundUser){
-        if(!err){
-            if(foundUser){
-                if(bcrypt.compareSync(req.body.password, foundUser.password)){
-                    console.log('user match found! logging in....'+foundUser);
-                    res.render('secrets');
-                }
-                else{
-                    console.log('incorrect password entered');
-                    res.redirect('/login');
-                }
-            }
-            else{
-                console.log('user match not found!');
-                res.redirect('/login');
-            }
-        }else{
-            res.send(err);
-        }
+  let user = new User({
+    username:req.body.email,
+    password:req.body.password
+  });
+    req.login(user,function(err){
+      if(err){
+        res.render('/failure',{msg:err});
+      }else{
+        res.redirect('/secrets');
+      }
     })
+});
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 port = 3000;
